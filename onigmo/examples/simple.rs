2230f081f5c2547a60d462ebf480b0a49b1c869a
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2017-10-12 19:07:49 +0900

    tweak code

--- onigmo/examples/simple.rs
+++ onigmo/examples/simple.rs
@@ -4,9 +4,9 @@ fn main() {
     let mut reg = onig::Regex::new("a(.*)b|[e-f]+".to_string()).unwrap();
     let s = "zzzzaffffffffb";
     match reg.search(s) {
-        Some(reg) => {
+        Some(ret) => {
             use std::str::from_utf8;
-            for (beg, end) in reg.positions() {
+            for (beg, end) in ret.positions() {
                 println!("{}", from_utf8(&s.as_bytes()[beg..end]).unwrap());
             }
         }
