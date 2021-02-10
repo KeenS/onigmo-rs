97beec9dec02595e616dc74d91fe116c4e3ca892
κeen <3han5chou7@gmail.com>
2021-02-10 15:56:59 +0900

    migrate to 2018 edition

--- onigmo-sys/build.rs
+++ onigmo-sys/build.rs
@@ -1,25 +1,20 @@
-extern crate bindgen;
-
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // onigmoの共有ライブラリを使うことをcargoがrustcに伝えるように伝えます
+    // onigmoの共有ライブラリを使うことをcargoがrustcに伝えるように伝える
     println!("cargo:rustc-link-lib=onigmo");
 
-    // `binden::Builder`がbindgenを使うときのメインのエントリーポイントです
+    // `binden::Builder`がbindgenを使うときのメインのエントリーポイント
     // オプションを設定できます。
     let bindings = bindgen::Builder::default()
-        // featureを要求したりnightlyでしか動かないような
-        // unstableなコード使いません
-        //.rust_target(LATEST_STABLE_RUST)
-        // バインディングを作る基になるヘッダファイルです
+        // バインディングを作る基になるヘッダファイル
         .header("wrapper.h")
-        // ビルダーを完了してバインディングを生成します
+        // ビルダーを完了してバインディングを生成する
         .generate()
         .expect("Unable to generate bindings");
 
-    // バインディングを`$OUT_DIR/bindings.rs`に書き出します。
+    // バインディングを`$OUT_DIR/bindings.rs`に書き出す。
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
